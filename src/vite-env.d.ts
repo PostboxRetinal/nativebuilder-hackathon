bf35f35920f3fef0aa5f99bbbd24e24e0c0db2ec
PostboxRetinal <78338192+PostboxRetinal@users.noreply.github.com>
2026-08-06 08:46:56 +0000

    fix(bun): added missing npm checks

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="vite/client" />
+
+declare module '*.svg?react' {
+  import * as React from 'react';
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+
+declare module '*.svg?import&react' {
+  import * as React from 'react';
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
